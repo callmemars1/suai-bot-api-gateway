dbc25676ae2897ec487a255d320cad44d9fdec42
callmemars1 <callmemars111@gmail.com>
2022-05-05 23:32:34 +0300

    added dockerfile

--- Program.cs
+++ Program.cs
@@ -22,6 +22,9 @@ app.Map("/", () =>
     return $"This is main page\nAdd \"/api.[service].[method]/\" to address string to use api\n";
 });
 
+
+Console.WriteLine("Some text to check");
+
 //app.UseHttpsRedirection();
 
 app.UseAuthorization();
