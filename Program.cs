e332f557b46af772a7deaf266e6d24a90369b548
callmemars1 <callmemars111@gmail.com>
2022-06-02 01:22:19 +0300

    added envs

--- Program.cs
+++ Program.cs
@@ -3,21 +3,20 @@ using suai_api.Models.Timetable;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.WebHost.UseSetting("https_port", "443");
-
 builder.Configuration.AddCommandLine(args);
+builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 
 builder.Services.AddSingleton<ITimetableProvider>((x) =>
 {
-    var serviceUrl = builder.Configuration.GetValue<string>("schedule_service_addr");
+    var serviceUrl = builder.Configuration.GetValue<string>("TIMETABLE_SERVICE_ADDR");
     return new TimetableProviderFromGRPCService(x.GetRequiredService<ILogger<TimetableProviderFromGRPCService>>(), serviceUrl);
 });
 
 builder.Services.AddSingleton<ITeacherInfoProvider>((x) =>
 {
-    var serviceUrl = builder.Configuration.GetValue<string>("schedule_service_addr");
+    var serviceUrl = builder.Configuration.GetValue<string>("TEACHER_SERVICE_ADDR");
     return new TeacherInfoProviderFromGRPCService(x.GetRequiredService<ILogger<TeacherInfoProviderFromGRPCService>>(), serviceUrl);
 });
 
@@ -28,9 +27,6 @@ app.Map("/", () =>
     return $"This is main page\nAdd \"/[university_name].[service].[method]/\" to address string to use api\n";
 });
 
-//app.UseHttpsRedirection();
-
-app.UseAuthorization();
 
 app.MapControllers();
 
